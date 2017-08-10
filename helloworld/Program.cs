ee2ffbbce41c96e4f9a554a37e8359c58a12090f
Chingiz <olzhabaev.sh@outlook.com>
2017-08-10 16:30:38 +0600

    helloWorldApplicationAdded

---
+++ helloworld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+            
+            Console.ReadKey();
+        }
+    }
+}
