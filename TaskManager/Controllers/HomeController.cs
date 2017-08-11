0b03eee22890afb460e544519def8dc5d53e069e
Chingiz <olzhabaev.sh@outlook.com>
2017-08-11 15:50:59 +0600

    addTaskManager

---
+++ TaskManager/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            
+            var products = new List<Product>();
+
+            products.Add(new Product()
+            {
+                Title = "Cola",
+                Amount = 24
+            });
+            products.Add(new Product()
+            {
+                Title = "Sprite",
+                Amount = 12
+            });
+            products.Add(new Product()
+            {
+                Title = "Fanta",
+                Amount = 21
+            });
+            
+            return View(products);            
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ TaskManager/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace TaskManager.Models
+{
+    public class Product 
+    {
+        public string Title { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
